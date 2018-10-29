9be7e9ee50518007ea610f32d934fbc6478b3d2e
Aman Khoja <aak171730@utdallas.edu>
2018-10-29 12:02:20 -0500

    Script 1.1 and Log updated2

--- script1.sh
+++ script1.sh
@@ -1,3 +1,25 @@
-!#bin/bash
+#!/bin/bash
+
+#for entry in "$search_dir"/*
+#do
+#echo "$entry"
+#done
+
+
+echo "Enter directory name"
+read dirname
+
+if [ ! -d "$dirname" ]
+    then
+        echo "Directory doesn't exist. Creating now"
+        mkdir ./$dirname;
+        echo "Directroy '$dirname' created"
+    if[[! -e /$dirname/file10.txt]]
+        touch /$dirname/file10.txt
+        echo "File created"
+
+    else
+        echo "File exists"
+fi
 
 
